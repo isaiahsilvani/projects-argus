11c416fa2ca7410511f6dc1f02e1d497ecd64b83
isaiahsilvani <isaiahsilvani@outlook.com>
2021-06-01 13:10:39 -0400

    set io connect/disconnect with user util

--- chatroom/client/src/components/Chatroom/Chatroom.tsx
+++ chatroom/client/src/components/Chatroom/Chatroom.tsx
@@ -1,10 +1,15 @@
 import { SocketConstructorOpts } from 'net';
-import React, { useEffect, useRef} from 'react'
+import React, { useEffect, useState} from 'react'
 import io, { Socket } from 'socket.io-client'
 
 
 
 const Chatroom: React.FC = () => {
+  const [connected, setConnected] = useState(false)
+  const [username, setUsername] = useState("")
+  const [connectedUsers, setConnectedUsers] = useState([] as {id: string, username: string}[])
+
+
   const socket = io("http://localhost:1337")
 
   useEffect(() => {
--- chatroom/server/src/server.ts
+++ chatroom/server/src/server.ts
@@ -1,8 +1,9 @@
 import express from 'express'
 import http from 'http'
-import { Server } from 'socket.io'
+import { Server, Socket } from 'socket.io'
 import config from './config/config'
 import logging from './config/logging'
+import { getUsers, userJoin, userLeave} from './util/user'
 
 const NAMESPACE = 'Server'
 
@@ -23,8 +24,21 @@ const server = http.createServer(app)
 //socketio connection...
 const io = new Server(server, {cors: {origin: "http://localhost:3000"}})
 
-io.on("connection", () => {
-  console.log("client connected")
+io.on("connection", (socket) => {
+  socket.join("mychat")
+
+  socket.on("handle-connection", (username: string) => {
+    if (!userJoin(socket.id, username)) {
+        socket.emit("username-taken")
+    } else {
+        socket.emit('username-submitted')
+        io.to("myChat").emit("get-connected-users", getUsers())
+    }
+  })
+
+  socket.on("disconnect", () => {
+    userLeave(socket.id)
+  })
 })
 // server listen...
 const PORT = config.server.port
