c41761015c33a5ad90bef2ac775b357bc283072d
isaiahsilvani <isaiahsilvani@outlook.com>
2021-06-01 16:53:39 -0400

    show connected users

--- chatroom/client/src/components/ConnectedUsers/ConnectedUsers.tsx
+++ chatroom/client/src/components/ConnectedUsers/ConnectedUsers.tsx
@@ -5,19 +5,29 @@ import { bindActionCreators } from 'redux';
 import { store } from '../../state/store'
 import User from './User';
 
+type IUser = {
+  id: string,
+  username: string
+}
+
 
 const ConnectedUsers = () => {
     const dispatch = useDispatch()
     const { setUsername } = bindActionCreators(actionCreators, dispatch)
     const connectedUsers = useSelector((store: State) => store.connectedUsers)
+    const connectedUser = useSelector((store: State) => store.connectedUser)
     console.log({ connectedUsers })
 
     return (
       <div className="connected-users">
         <h2>Connected Users</h2>
-          
         <ul>
-          <User/>
+          {connectedUsers.map((user: any) => (
+            <div key={user.id}>
+              {console.log(user.username)}
+              <h5>{user.username}</h5>
+            </div>
+          ))}
         </ul>
       </div>
     );
--- chatroom/client/src/components/ConnectedUsers/User.tsx
+++ chatroom/client/src/components/ConnectedUsers/User.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 
 interface UserProps {
-
+  id?: string,
+  key: string,
+  user?: string
 }
 
 const User: React.FC<UserProps> = ({}) => {
