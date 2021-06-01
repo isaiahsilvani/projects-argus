1b23bdb40a6bec237eaa102d77f9c25a74430b85
isaiahsilvani <isaiahsilvani@outlook.com>
2021-06-01 13:18:27 -0400

    set user socket listeners

--- chatroom/client/src/components/Chatroom/Chatroom.tsx
+++ chatroom/client/src/components/Chatroom/Chatroom.tsx
@@ -1,6 +1,7 @@
 import { SocketConstructorOpts } from 'net';
 import React, { useEffect, useState} from 'react'
 import io, { Socket } from 'socket.io-client'
+import { toast, ToastContainer } from 'react-toastify'
 
 
 
@@ -9,18 +10,38 @@ const Chatroom: React.FC = () => {
   const [username, setUsername] = useState("")
   const [connectedUsers, setConnectedUsers] = useState([] as {id: string, username: string}[])
 
-
   const socket = io("http://localhost:1337")
 
   useEffect(() => {
-    
-  }, []);
+    if(socket){
 
-    return (
-      <div className="chatroom">
-        CHATROOM HERE
-      </div>
-    );
+      socket.on("username-taken", () => {
+        toast.error("Username is taken")
+      })
+
+      socket.on("username-submitted", () => {
+        setConnected(true)
+      })
+
+      socket.on("get-connected-users", (connectedUsers: {id: string, username: string}[]) => {
+        setConnectedUsers(connectedUsers.filter(user => user.username !== username))
+        console.log(connectedUsers)
+      })
+    }
+  }, [])
+
+  const handleConnection = () => {
+    if(socket){
+      socket.emit("handle-connection")
+    }
+  }
+
+  return (
+    <div className="chatroom">
+      CHATROOM HERE
+      <ToastContainer position="bottom-right" />
+    </div>
+  );
 }
 
 export default Chatroom
