5f3290f8e562ad31915ce0ee78fecb8bc15d2860
isaiahsilvani <isaiahsilvani@outlook.com>
2021-06-02 10:06:53 -0400

    basic ui layout

--- quiz-app/src/App.tsx
+++ quiz-app/src/App.tsx
@@ -1,10 +1,34 @@
 import React from 'react';
 import logo from './logo.svg';
+// Components
+import QuestionCard from './components/QuestionCard'
 
 function App() {
+  // make the API call when trivia game is started
+  const startQuiz = async () => {
+
+  }
+  // check answer
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
+
+  }
+
+  // next question
+  const nextQuestion = () => {
+
+
+  }
+
+
   return (
     <div className="App">
-      APPLICATION
+      <h1>The Ultimate Quiz</h1>
+      <button className="start" onClick={startQuiz}>Start</button>
+      <p className="score">Score: </p>
+      <p>Loading Questions...</p>
+      <QuestionCard />
+
+      <button className="next-btn" onClick={nextQuestion}>Next Question</button>
     </div>
   );
 }
