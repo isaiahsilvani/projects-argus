d420ec5300baaaf2e2e1371cbb79b83212322ee5
isaiahsilvani <isaiahsilvani@outlook.com>
2021-06-02 20:56:45 -0400

    fetch userscores from database

--- quiz-app/src/components/UserScoreList/UserScoreList.tsx
+++ quiz-app/src/components/UserScoreList/UserScoreList.tsx
@@ -1,9 +1,14 @@
 import React, { useEffect } from 'react'
+import * as userscore_api from '../../services/userscore-api'
 
 const UserScoreList = () => {
 
     useEffect(() => {
       console.log('trigger API request')
+      userscore_api.getUserScores()
+      .then(data => {
+        console.log('set data: ', data)
+      })
     }, []);
 
     return (
