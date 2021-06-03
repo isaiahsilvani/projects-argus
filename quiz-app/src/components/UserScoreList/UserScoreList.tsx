22fd012a4af7ea4f0797dae92e7bfdaadba86a7d
isaiahsilvani <isaiahsilvani@outlook.com>
2021-06-02 21:04:01 -0400

    set userscores in state

--- quiz-app/src/components/UserScoreList/UserScoreList.tsx
+++ quiz-app/src/components/UserScoreList/UserScoreList.tsx
@@ -1,14 +1,23 @@
 import React, { useEffect } from 'react'
 import * as userscore_api from '../../services/userscore-api'
+import { bindActionCreators } from 'redux';
+import { actionCreators } from '../../state/';
+import { useSelector, useDispatch } from 'react-redux'
 
 const UserScoreList = () => {
+    const dispatch = useDispatch()
+    const { setUserScores } = bindActionCreators(actionCreators, dispatch)
+    const userScores = useSelector((state: any) => state.userscores)
 
+    // set userscores in state onmount only if state is currently empty
     useEffect(() => {
-      console.log('trigger API request')
-      userscore_api.getUserScores()
-      .then(data => {
-        console.log('set data: ', data)
-      })
+      if(userScores.length === 0){
+        userscore_api.getUserScores()
+        .then(data => {
+          setUserScores(data)
+        })
+      }
+
     }, []);
 
     return (
